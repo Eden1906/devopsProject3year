aa8d1ea6b693e399070676b6b684cc258a72e10e
Eden <Edenlevy12@gmail.com>
2023-05-29 22:36:37 +0300

    added test and design

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -9,3 +9,17 @@ describe('test suite 1:', () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
+describe('POST/register', () => {
+  test('should return 200 when form is submitted', async () => {
+    const res = await request(app)
+      .post('/register')
+      .send({
+        name: 'test',
+        grade1: '100',
+        grade2: '90',
+        grade3: '80',
+      });
+    expect(res.statusCode).toEqual(200);
+  });
+});
